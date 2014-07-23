11563d209cb1284910d235740f95659d9a6113ea
anionyx <rey@innovativeops.co>
2014-07-23 22:16:12 +0000

    Wiki app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wikifool::Application.config.secret_key_base = '92b505f9f22f5a9b38cdb897abef655af99e40d1079b76e2636a55a15483cb0ba2e7bd89adc11bde4bbaec7d952685cb5009903ec7db2bd1c510c5f9f5dae657'
